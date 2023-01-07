eeaa92749bf56e973c08d857db668280e0a3e6f1
Chasith-Randima <chasith19@gmail.com>
2023-01-07 18:41:52 +0530

    created crud functions related to wanted model and routes related to it.

--- controllers/wantedController.js
+++ controllers/wantedController.js
@@ -1,6 +1,8 @@
 const Wanted = require("./../models/wantedModel");
 const factory = require("./handlerFactory");
 
+// wanted model
+
 exports.getAllWanted = factory.getAll(Wanted);
 exports.getOneWanted = factory.getOne(Wanted);
 exports.createOneWanted = factory.createOne(Wanted, "Wanted");
