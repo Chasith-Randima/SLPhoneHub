05ef4ff4fb38af0756db810f1f8c3bdb9c8f2ffd
Chasith-Randima <chasith19@gmail.com>
2023-01-14 22:52:20 +0530

    index wasnt added...

---
+++ index.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+// console.log(DB);
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    //useFindModify:false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful..");
+  });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
